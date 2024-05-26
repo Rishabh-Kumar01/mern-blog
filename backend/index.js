65460b6a5d190412cf8db233785c301d1edf5467
Rishabh kumar <rishabh1teotia@gmail.com>
2024-05-26 19:06:36 +0530

    created a middleware for error and utils function for custom error.

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import { stat } from "fs";
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ mongoose
   });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!!");
@@ -27,3 +28,14 @@ app.use("/api/user", userRoutes);
 
 // Auth
 app.use("/api/auth", authRoutes);
+
+// middleware error
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
