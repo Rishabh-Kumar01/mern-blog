323a48f8e38ae4b3a7ce31ce3a927b57d721b3b3
Rishabh kumar <rishabh1teotia@gmail.com>
2024-05-30 19:18:36 +0530

    Created post model, create post api route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -29,6 +30,9 @@ app.use("/api/user", userRoutes);
 
 // Auth
 app.use("/api/auth", authRoutes);
+
+// Post
+app.use("/api/post", postRoutes);
 
 // middleware error
 app.use((err, req, res, next) => {
