c86812017432541635fd851e5f8fde6044d095af
Rishabh kumar <rishabh1teotia@gmail.com>
2024-05-31 01:39:14 +0530

    Created getPosts api route with filters.

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,8 +1,8 @@
+import exp from "constants";
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const createPost = async (req, res, next) => {
- 
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not allowed to create a post"));
   }
@@ -26,6 +26,47 @@ export const createPost = async (req, res, next) => {
   try {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchItem && {
+        $or: [
+          { title: { $regex: req.query.searchItem, $options: "i" } },
+          { content: { $regex: req.query.searchItem, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({ posts, totalPosts, lastMonthPosts });
   } catch (error) {
     next(error);
   }
