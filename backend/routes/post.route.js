541d74ba2df4204f9e9f35c86de67709a141f34d
Rishabh kumar <rishabh1teotia@gmail.com>
2024-06-06 00:04:48 +0530

    Edit functionality of post

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import {
+  createPost,
+  getPosts,
+  deletePost,
+  updatePost,
+} from "../controllers/post.controller.js";
 import { verifyUser } from "../utils/VerifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createPost);
 router.get("/getposts", getPosts);
+router.delete("/delete/:postId/:userId", verifyUser, deletePost);
+router.put("/update/:postId/:userId", verifyUser, updatePost);
 
 export default router;
