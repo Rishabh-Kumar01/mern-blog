e51a1c4a927a8dd3ad6bba75a75ba314289c9aeb
Rishabh kumar <rishabh1teotia@gmail.com>
2024-06-06 20:24:17 +0530

    About, Projects and add pre installation for deployment

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose
   .catch((err) => {
     console.log("Error: ", err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -37,6 +40,13 @@ app.use("/api/posts", postRoutes);
 
 // Comment
 app.use("/api/comment", commentRoutes);
+
+// Serve static assets if in production
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname), "client", "dist", "index.html");
+});
 
 // middleware error
 app.use((err, req, res, next) => {
