1bbc81dacde108ae1a5701c7877520fba56a8ed3
Rishabh kumar <rishabh1teotia@gmail.com>
2024-06-06 19:51:31 +0530

    Added comments section of the sidebar

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -5,6 +5,7 @@ import {
   deleteComment,
   editComment,
   getPostComments,
+  getcomments,
   likeComment,
 } from "../controllers/comment.controller.js";
 
@@ -15,5 +16,6 @@ router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyUser, likeComment);
 router.put("/editComment/:commentId", verifyUser, editComment);
 router.delete("/deleteComment/:commentId", verifyUser, deleteComment);
+router.get("/getcomments", verifyUser, getcomments);
 
 export default router;
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -5,6 +5,7 @@ import {
   HiDocumentText,
   HiOutlineUserGroup,
   HiUser,
+  HiAnnotation,
 } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signOutSuccess } from "../redux/user/userSlice";
@@ -67,15 +68,26 @@ function DashSidebar() {
           )}
 
           {currentUser.isAdmin && (
-            <Link to="/dashboard?tab=users">
-              <Sidebar.Item
-                active={tab === "users"}
-                icon={HiOutlineUserGroup}
-                as="div"
-              >
-                Users
-              </Sidebar.Item>
-            </Link>
+            <>
+              <Link to="/dashboard?tab=users">
+                <Sidebar.Item
+                  active={tab === "users"}
+                  icon={HiOutlineUserGroup}
+                  as="div"
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              <Link to="/dashboard?tab=comments">
+                <Sidebar.Item
+                  active={tab === "comments"}
+                  icon={HiAnnotation}
+                  as="div"
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
           )}
 
           <Sidebar.Item
