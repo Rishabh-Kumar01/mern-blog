0c63ca6e0bf32ad365e0fd37a5913d534dd36d3c
Rishabh kumar <rishabh1teotia@gmail.com>
2024-06-06 18:27:55 +0530

    show comments

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,7 +4,8 @@ import {
   updateUser,
   deleteUser,
   signoutUser,
-  getUsers
+  getUsers,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/VerifyUser.js";
 
@@ -14,6 +15,7 @@ router.get("/test", test);
 router.put("/update/:userId", verifyUser, updateUser);
 router.delete("/delete/:userId", verifyUser, deleteUser);
 router.post("/signout", signoutUser);
-router.get("/getusers", verifyUser, getUsers)
+router.get("/getusers", verifyUser, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
